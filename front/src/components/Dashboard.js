5cfe998e98bc90b711364242ed9c1c6a3393cf7a
Fabien Raymond <voortexxx@gmail.com>
2018-12-19 17:54:29 +0100

    some style and animation

--- front/src/components/Dashboard.js
+++ front/src/components/Dashboard.js
@@ -1,39 +1,47 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, Row } from "reactstrap";
 import data from "./data";
 import "./Dashboard.css";
 import { Grid } from "mauerwerk";
+import PlayButton from "./PlayButton";
 
 class Dashboard extends Component {
   render() {
     return (
-      <Grid
-        className="grid"
-        // Arbitrary data, should contain keys, possibly heights, etc.
-        data={data}
-        // Key accessor, instructs grid on how to fet individual keys from the data set
-        keys={d => d.name}
-        // Can be a fixed value or an individual data accessor
-        heights={d => d.height}
-        // Number of columns
-        columns={2}
-      >
-        {(data, maximized, toggle) => (
-          <div
-            className="cell"
-            style={{ backgroundImage: data.css }}
-            onClick={toggle}
-          >
-            {maximized && (
-              <div className="details">
-                <div className="circle" style={{ background: data.css }} />
-                <h1>{data.name}</h1>
-                <p>{data.description}</p>
-              </div>
-            )}
-            {!maximized && <div className="default">{data.name}</div>}
-          </div>
-        )}
-      </Grid>
+      <Row>
+        <Grid
+          className="grid"
+          // Arbitrary data, should contain keys, possibly heights, etc.
+          data={data}
+          // Key accessor, instructs grid on how to fet individual keys from the data set
+          keys={d => d.name}
+          // Can be a fixed value or an individual data accessor
+          heights={d => d.height}
+          // Number of columns
+          columns={2}
+        >
+          {(data, maximized, toggle) => (
+            <div
+              className="cell"
+              style={{ backgroundImage: data.css }}
+              onClick={toggle}
+            >
+              {maximized && (
+                <div className="details">
+                  <h1>{data.name}</h1>
+                  <p>{data.description}</p>
+
+                  <div className="d-flex justify-content-center">
+                    <PlayButton />
+                  </div>
+                </div>
+              )}
+              {!maximized && <div className="default">{data.name}</div>}
+            </div>
+          )}
+        </Grid>
+      </Row>
     );
   }
 }
---
+++ front/src/components/PlayButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PlayButton.css";
+
+const PlayButton = () => {
+  return (
+    <button class="play-button hvr-buzz">
+      <span>Jouer au jeu</span>
+    </button>
+  );
+};
+
+export default PlayButton;
