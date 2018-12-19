922943c3b3208a6fd83ed046bdddb920ca8e365c
Mathieu THOMAS <kawacke@gmail.com>
2018-12-19 16:42:05 +0100

    initialise router

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Container, Row } from "reactstrap";
+import { Container } from "reactstrap";
+import { Switch, Route } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -9,10 +10,10 @@ class App extends Component {
   render() {
     return (
       <Container fluid className="App position-relative">
-        <Row>
-          <Home />
-          <Footer />
-        </Row>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
       </Container>
     );
   }
--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import "./Navbar.css";
 
 export class Navbar extends Component {
   render() {
     const menu = [
-      { name: "# Jouer", path: "/jouer" },
+      { name: "# Jouer", path: "/tableau-de-jeux" },
       { name: "# Profil", path: "/profil" },
       { name: "# Classement", path: "/classement" }
     ];
     return (
       <div className="main_container_navbar d-flex flex-column align-items-end">
         {menu.map((item, index) => (
-          <div key={index} className="item_navbar my-3 py-1">
-            <h3 className="p-0 m-0">{item.name}</h3>
-          </div>
+          <Link key={index} className="item_navbar my-3 py-1" to={item.path}>
+            {item.name}
+          </Link>
         ))}
       </div>
     );
