7eed96a7598875c01b58b96017cc30cbda6f63dc
Fabien Raymond <voortexxx@gmail.com>
2018-12-20 16:14:28 +0100

    menu with animation

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Spring } from "react-spring";
+import { Row, Col } from "reactstrap";
 
 import "./Navbar.css";
 
@@ -7,17 +9,46 @@ export class Navbar extends Component {
   render() {
     return (
       <div className="main_container_navbar d-flex flex-column align-items-end">
-        <Link className="item_navbar my-3 py-1" to="/tableau-de-jeux">
-          <i class="fas fa-gamepad" /> Jouer
-        </Link>
+        <Spring
+          from={{ opacity: 0, transform: "translate3d(100px,0,0)" }}
+          to={{ opacity: 1, transform: "translate3d(0px,0,0)" }}
+          config={{ delay: 3000 }}
+        >
+          {props => (
+            <div style={props}>
+              <Link className="item_navbar my-3 py-1" to="/tableau-de-jeux">
+                <i className="fas fa-gamepad" /> Jouer
+              </Link>
+            </div>
+          )}
+        </Spring>
 
-        <Link className="item_navbar my-3 py-1" to="/profil">
-          <i class="fas fa-user" /> Profil
-        </Link>
-
-        <Link className="item_navbar my-3 py-1" to="/classement">
-          <i class="fas fa-trophy" /> Classement
-        </Link>
+        <Spring
+          from={{ opacity: 0, transform: "translate3d(100px,0,0)" }}
+          to={{ opacity: 1, transform: "translate3d(0px,0,0)" }}
+          config={{ delay: 3200 }}
+        >
+          {props => (
+            <div style={props}>
+              <Link className="item_navbar my-3 py-1" to="/profil">
+                <i className="fas fa-user" /> Profil
+              </Link>
+            </div>
+          )}
+        </Spring>
+        <Spring
+          from={{ opacity: 0, transform: "translate3d(100px,0,0)" }}
+          to={{ opacity: 1, transform: "translate3d(0px,0,0)" }}
+          config={{ delay: 3400 }}
+        >
+          {props => (
+            <div style={props}>
+              <Link className="item_navbar my-3 py-1" to="/ranking">
+                <i className="fas fa-trophy" /> Classement
+              </Link>
+            </div>
+          )}
+        </Spring>
       </div>
     );
   }
