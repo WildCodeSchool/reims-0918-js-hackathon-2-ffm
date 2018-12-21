a900ed9d467630c4fc53d7744f85fe862791b6ab
Mathieu THOMAS <kawacke@gmail.com>
2018-12-21 04:19:57 +0100

    add stockage score memory

--- front/src/components/memory/card/GameOver.js
+++ front/src/components/memory/card/GameOver.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import { Row } from "reactstrap"
+import React, { Component } from "react";
+import { Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const GameOver = ({ restartGame, timeLeft }) => (
-  <div>
-    <Row className="justify-content-center">
-      {!timeLeft === 0 ?
-        <h2 style={{ fontFamily: "cobolbold" }}>Bravo !</h2> :
-        <h2 style={{ fontFamily: "cobolbold" }}>Perdu !</h2>
-      }
-    </Row>
+class GameOver extends Component {
+  componentDidMount() {
+    if (this.props.timeLeft !== 0) {
+      this.props.stop();
+    }
+  }
+  render() {
+    const { restartGame, timeLeft } = this.props;
+    return (
+      <div>
+        <Row className="justify-content-center">
+          {!timeLeft === 0 ? (
+            <h2 style={{ fontFamily: "cobolbold" }}>Bravo !</h2>
+          ) : (
+            <h2 style={{ fontFamily: "cobolbold" }}>Perdu !</h2>
+          )}
+        </Row>
 
-    <Row className="justify-content-center">
-      <Link className="back-button" to="/tableau-de-jeux">
-        Retour
-        </Link>
-      <button className="back-button" onClick={restartGame}>Rejouer ?</button>
-    </Row>
-  </div>
-)
+        <Row className="justify-content-center">
+          <Link className="back-button" to="/tableau-de-jeux">
+            Retour
+          </Link>
+          <button className="back-button" onClick={restartGame}>
+            Rejouer ?
+          </button>
+        </Row>
+      </div>
+    );
+  }
+}
 
-export default GameOver
+export default GameOver;
